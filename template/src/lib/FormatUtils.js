91fa066c94e46d555e6f216ec963a93ce83f46a2
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2020-06-10 17:01:02 +0200

    Shouldn't have checked in template

--- template/src/lib/FormatUtils.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ReactMarkdown from 'react-markdown'
-
-const ffmd = (mdtext) => {
-    return <ReactMarkdown source={mdtext} />
-}
-
-export {ffmd}
